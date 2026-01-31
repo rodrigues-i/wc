0f7722e3ac5b23e6064595498c3344dbabe0b634
Lucas Rodrigues <rodrigues-i@outlook.com>
2026-01-31 16:46:21 -0300

    Change solution structure

--- Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace wc;
-
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Hello World!");
-    }
-}
---
+++ wc/Program.cs
@@ -0,0 +1,26 @@
+﻿using wc.core.Contracts;
+using wc.core.Services;
+
+namespace wc;
+
+public class Program
+{
+    private static IArgumentsValidator _argumentsValidator = new ArgumentsValidator();
+    public static void Main(string[] args)
+    {
+        if (args.Count() == 0)
+        {
+            Console.WriteLine("Missing command line arguments.");
+            return;
+        }
+
+        if(args.Count() == 2 && args[0] == "-c")
+        {
+            bool fileExists = _argumentsValidator.FileExists(args[1]);
+            if(!fileExists)
+            {
+                Console.WriteLine($"Could not find file {args[1]}");
+            }
+        }
+    }
+}
