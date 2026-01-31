9fe28ed7e8f2c29f9a1c36afe576919f00d8023c
Lucas Rodrigues <rodrigues-i@outlook.com>
2026-01-31 16:46:05 -0300

    Create project wc.core

---
+++ wc.core/Contracts/IArgumentsValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wc.core.Contracts
+{
+    public interface IArgumentsValidator
+    {
+        bool FileExists(string filePath);
+    }
+}
---
+++ wc.core/Services/ArgumentsValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wc.core.Contracts;
+
+namespace wc.core.Services
+{
+    public class ArgumentsValidator : IArgumentsValidator
+    {
+        public bool FileExists(string filePath)
+        {
+            return File.Exists(filePath);
+        }
+    }
+}
